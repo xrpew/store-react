2451393983e345d5bcc1f7a4bc329538aa19dd7a
Sergio Perez <spergio8@gmail.com>
2023-01-23 20:19:08 -0300

    [UPDATE]

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath:'/'
+        publicPath:'./'
     },
     devServer:{
         static: path.join(__dirname, "dist"),
